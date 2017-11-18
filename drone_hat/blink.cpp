810b63e5b184c2f1fc54d5f84abb14384d1102dd
feliwir <stephan.vedder@gmail.com>
2017-11-18 14:21:27 +0000

    got serial communication working

--- drone_hat/blink.cpp
+++ drone_hat/blink.cpp
@@ -1,22 +1,18 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
-
-
-SoftwareSerial serial(0, 1);
 
 void setup() 
 {
-  serial.begin(9600);
+  Serial.begin(9600);
   pinMode(2,OUTPUT);
-  serial.println("Setup ready...");
+  Serial.println("Setup ready...");
 }
 
 void loop() 
 {
-  serial.println("HIGH");
+  Serial.println("HIGH");
   digitalWrite(2,HIGH);
   delay(1000);
-  serial.println("LOW");
+  Serial.println("LOW");
   digitalWrite(2,LOW);
   delay(1000);
 }
--- listen.py
+++ listen.py
@@ -2,12 +2,13 @@ import serial
 import time
 import sys
 
-ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=10)
+ser = serial.Serial("/dev/ttyS0", 9600, timeout=10)
 
 while 1:
 	try:
-		text = ser.read(1)
-		print (text)
+		text = ser.readline()[:-2].decode().strip()
+		if (len(text) > 0):
+			print (text)
 		time.sleep(0.05)
 	except:
 		print("Timeout")
