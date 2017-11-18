d72381064914f79ce925ba708f006241ebd66458
Stephan Vedder <stephan.vedder@gmail.com>
2017-11-18 15:13:02 +0100

    simplify listen script

--- listen.py
+++ listen.py
@@ -6,9 +6,9 @@ ser = serial.Serial("/dev/ttyAMA0", 9600, timeout=10)
 
 while 1:
 	try:
-		text = ser.readlin()[:-2].decode().strip()
-		if (len(text) > 0):
-			print (text)
+		text = ser.read(1)
+		print (text)
 		time.sleep(0.05)
 	except:
+		print("Timeout")
 		continue
