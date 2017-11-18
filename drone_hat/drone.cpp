4feac2910ef7608afbba6af0697b0ba538521dc4
Tarcontar <michael-schnabel@gmx.net>
2017-11-19 00:07:32 +0100

    work on motor controlling

--- drone_hat/drone.cpp
+++ drone_hat/drone.cpp
@@ -2,10 +2,24 @@
 #include <BMI160Gen.h>
 #include <BME280.h>
 #include <org1411.h>
+#include <Servo.h>
 
 #define BMI160_PIN 8
 #define BME280_PIN 7
 #define GPS_PIN 5
+
+#define MIN_THROTTLE 1000
+#define MAX_THROTTLE 1700
+
+int FLSpeed = 0;
+int FRSpeed = 0;
+int BRSpeed = 0;
+int BLSpeed = 0;
+
+Servo motorFL;
+Servo motorFR;
+Servo motorBR;
+Servo motorBL;
 
 BME280 bme(BME280_PIN);
 Org1411 gps;
@@ -65,11 +79,52 @@ void printGPS()
 	}
 }
 
+void setupMotors()
+{
+	FLSpeed = MIN_THROTTLE;
+	FRSpeed = MIN_THROTTLE;
+	BRSpeed = MIN_THROTTLE;
+	BLSpeed = MIN_THROTTLE;
+	
+	motorFL.attach(14);
+	motorFR.attach(13);
+	motorBR.attach(10);
+	motorBL.attach(1);
+	
+	int min = 1000;
+	int max = 2000;
+	motorFL.writeMicroseconds(min);
+	motorFR.writeMicroseconds(min);
+	motorBR.writeMicroseconds(min);
+	motorBL.writeMicroseconds(min);
+	delay(3000);
+	motorFL.writeMicroseconds(max);
+	motorFR.writeMicroseconds(max);
+	motorBR.writeMicroseconds(max);
+	motorBL.writeMicroseconds(max);
+	delay(2);
+	motorFL.writeMicroseconds(min);
+	motorFR.writeMicroseconds(min);
+	motorBR.writeMicroseconds(min);
+	motorBL.writeMicroseconds(min);
+}
+
+void startMotors()
+{
+	int val = 1200;
+	motorFL.writeMicroseconds(val);
+	motorFR.writeMicroseconds(val);
+	motorBR.writeMicroseconds(val);
+	motorBL.writeMicroseconds(val);
+}
+
 void setup() 
 {
 	Serial.begin(9600);
 	pinMode(2,OUTPUT);
 	Serial.println("Setup ready...");
+	
+	setupMotors();
 
 	if (!gps.begin(GPS_PIN))
 	{
@@ -110,6 +165,8 @@ void setup()
 		Serial.println("BME SUCCESSFULL");	
 		printBME();
 	}
+	
+	startMotors();
 }
 
 void loop() 
