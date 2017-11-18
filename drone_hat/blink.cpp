2fd32c5499ef72fe9aa4501c52793b76cccc234e
Stephan Vedder <stephan.vedder@gmail.com>
2017-11-18 03:12:29 +0100

    added clean script

--- drone_hat/blink.cpp
+++ drone_hat/blink.cpp
@@ -9,11 +9,17 @@ void loop(void);
 
 void setup() {
   pinMode(13,OUTPUT);
-  digitalWrite(13,HIGH);
+
 }
 
 void loop() {
   //_delay_ms(BLINK_DELAY_MS);   // delay  
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+  delay(1000);
+
+  
 }
 
 int main(void) {
