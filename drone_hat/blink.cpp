337718dc8d40b10d552ffd385d08da1ca99aa55c
Stephan Vedder <stephan.vedder@gmail.com>
2017-11-18 12:25:34 +0100

    Remove main, not allowed with arduino.h

--- drone_hat/blink.cpp
+++ drone_hat/blink.cpp
@@ -1,22 +1,14 @@
 #include <Arduino.h>
-#include <SoftwareSerial.h>
 
-#define BLINK_DELAY_MS 500
-
-void setup() {
-  pinMode(13,OUTPUT);
+void setup() 
+{
+  pinMode(2,OUTPUT);
 }
 
-void loop() {
-  //_delay_ms(BLINK_DELAY_MS);   // delay  
-  digitalWrite(13,HIGH);
+void loop() 
+{
+  digitalWrite(2,HIGH);
   delay(1000);
-  digitalWrite(13,LOW);
+  digitalWrite(2,LOW);
   delay(1000);
-}
-
-int main(void) {
-  setup();
-  while(true)
-    loop();
 }
