51a1ce76ad3a8d62b8540473332385c8072dbc7f
Tarcontar <michael-schnabel@gmx.net>
2017-11-24 18:30:38 +0100

    added bosch code snipped for testing

--- drone_hat/drone.cpp
+++ drone_hat/drone.cpp
@@ -1,100 +1,84 @@
 #include <Arduino.h>
-#include <stdint.h>
-//#include "io/pwmreceiver.h"
-//#include "io/sensors.h"
-//#include "math/pid.h"
-//#include "actuators/motors.h"
-#include "hat_pcb.h"
 
-#include "BMI160Gen.h"
+#include <SPI.h>
+#include <bmi160.h>
 
-#define PRESSURE_SEALEVEL 1013.25F
-
-//PWMReceiver receiver;
-
-/*
-Sensors sensors;
-Motors motors(FL_MOTOR, FR_MOTOR, BR_MOTOR, BL_MOTOR);
-
-PID rollPID(1.0);
-PID pitchPID(1.0);
-PID yawPID(1.0);
-*/
-
-//use these in debug only!!
-bool has_bmi = false;
-bool has_bme = false;
-bool has_gps = false;
-
-
-void setup() 
+/* Helper functions */
+int8_t spiTransfer(uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length);
+void delay_ms(uint32_t period)
 {
-	Serial.begin(9600);
-
-	Serial.println("#### STARTING SETUP ####");
-	pinMode(LED_STATUS, OUTPUT); 
-
-	//sensors.setup();
-	
-	
-	if (!BMI160.begin(BMI160GenClass::SPI_MODE, 8))
-	{
-		Serial.println("BMI160 setup failed");
-	}
-	else
-	{
-		BMI160.setGyroRate(25);
-		BMI160.setAccelerometerRate(25);
-		BMI160.setGyroRange(250);
-		BMI160.setAccelerometerRange(2);
-		delay(200);
-	}
-	
-	
-	//motors.setupESCs();
-	
-	//int8_t channels[6] = {CH1, CH2, CH3, CH4, CH5, CH6};
-	//receiver.setChannels(channels);
-
-	Serial.println("##### SETUP READY ######");
+  delay(period);
 }
 
-void loop() 
+/* Constants and variables */
+SPISettings set_bmi(1000000, MSBFIRST, SPI_MODE0);
+const uint8_t bmi160_cs = 8;
+struct bmi160_dev bmi_dev;
+String output;
+
+/* Application code execution starts here */
+void setup() {
+  /* Initialize debug serial communication at 9600 bits per second: */
+  Serial.begin(9600);
+
+  /* Initialize sensor interface */
+  pinMode(bmi160_cs, OUTPUT);
+  SPI.begin();
+
+  /* Set the inferace configurations */
+  bmi_dev.id = bmi160_cs;
+  bmi_dev.read = spiTransfer; // Duplex
+  bmi_dev.write = spiTransfer; // Duplex
+  bmi_dev.delay_ms = delay_ms;
+  bmi_dev.interface = BMI160_SPI_INTF;
+
+  /* Initialize the BMI160 */
+  output = "BMI160 init "  + String(bmi160_init(&bmi_dev)) + " , chip ID: " + String(bmi_dev.chip_id, HEX);
+  Serial.println(output);
+
+  /* Select the Output data rate, range of accelerometer sensor */
+  bmi_dev.accel_cfg.odr = BMI160_ACCEL_ODR_100HZ;
+  bmi_dev.accel_cfg.range = BMI160_ACCEL_RANGE_2G;
+  bmi_dev.accel_cfg.bw = BMI160_ACCEL_BW_NORMAL_AVG4;
+
+  /* Select the power mode of accelerometer sensor */
+  bmi_dev.accel_cfg.power = BMI160_ACCEL_NORMAL_MODE;
+
+  /* Select the Output data rate, range of Gyroscope sensor */
+  bmi_dev.gyro_cfg.odr = BMI160_GYRO_ODR_100HZ;
+  bmi_dev.gyro_cfg.range = BMI160_GYRO_RANGE_2000_DPS;
+  bmi_dev.gyro_cfg.bw = BMI160_GYRO_BW_NORMAL_MODE;
+
+  /* Select the power mode of Gyroscope sensor */
+  bmi_dev.gyro_cfg.power = BMI160_GYRO_NORMAL_MODE;
+
+  /* Set the sensor configuration */
+  output = "BMI160 set sensor config:  " + String(bmi160_set_sens_conf(&bmi_dev));
+  Serial.println(output);
+}
+
+/* The loop routine runs over and over again forever after the setup */
+void loop() {
+  bmi160_sensor_data accel;
+  bmi160_sensor_data gyro;
+
+  /* To read both Accel and Gyro data */
+  output = "R: " + String(bmi160_get_sensor_data((BMI160_ACCEL_SEL | BMI160_GYRO_SEL), &accel, &gyro, &bmi_dev));
+  output += " A: " + String(accel.x) + ", "  + String(accel.y) + ", " + String(accel.z);
+  output += " G: " + String(gyro.x) + ", "  + String(gyro.y) + ", " + String(gyro.z);
+  Serial.println(output);
+  delay(100);
+}
+
+int8_t spiTransfer(uint8_t cs, uint8_t reg_addr, uint8_t *reg_data, uint16_t length)
 {
-	digitalWrite(LED_STATUS, HIGH);
-	delay(500);
-	digitalWrite(LED_STATUS, LOW);
-	delay(500);
-	
-	
-	int x, y, z;
-	BMI160.readAccelerometer(x, y, z);
-	Serial.println(x);
-	
-	//sensors.setup();
+  SPI.beginTransaction(set_bmi);
+  digitalWrite(cs, LOW);
+  SPI.transfer(reg_addr);
+  for (uint8_t i = 0; i < length; i++)
+    reg_data[i] = SPI.transfer(reg_data[i]);
+  digitalWrite(8, HIGH);
+  SPI.endTransaction();
 
-	/*
-	//filter these receiver values?
-	int thrust = receiver.getChannel(PWMReceiver::CHANNEL::THRUST);
-	int rc_roll = receiver.getChannel(PWMReceiver::CHANNEL::ROLL);
-	int rc_pitch = receiver.getChannel(PWMReceiver::CHANNEL::PITCH);
-	int rc_yaw = receiver.getChannel(PWMReceiver::CHANNEL::YAW);
-	
-	rollPID.setTarget(rc_roll);
-	pitchPID.setTarget(rc_pitch);
-	yawPID.setTarget(rc_yaw);
-	
-	sensors.update();
-	//get accel values and map to 1000 - 2000
-	
-	int roll = 0;
-	int pitch = 0;
-	int yaw = 0;
-
-	int r = rollPID.update(roll);
-	int p = pitchPID.update(pitch);
-	int y = yawPID.update(yaw);
-	
-	motors.update(thrust, r, p, y);
-	*/
+  return 0;
 }
