941356de068c59198f629ababd9f6521f0dcc6d2
feliwir <stephan.vedder@gmail.com>
2017-11-26 00:32:38 +0000

    work on uploading routine

--- reset.py
+++ reset.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
+
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.OUT)
 GPIO.output(18, GPIO.LOW)
-time.sleep(1)
+time.sleep(2)
 GPIO.output(18, GPIO.HIGH)
 GPIO.cleanup()
