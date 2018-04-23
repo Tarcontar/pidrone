f02d382e828ff28233e74e7a9070db4d089ab114
feliwir <stephan.vedder@gmail.com>
2018-04-23 19:20:44 +0000

    got upload working again

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-unexport ()
-{
-	echo "unexport $1"
-	echo "$1" > /sys/class/gpio/unexport
-}
-#unexport 18
 bash compile.sh
 cd build
 make upload
-#set Reset pin high
-echo "18" > /sys/class/gpio/export
-echo "out" > /sys/class/gpio/gpio18/direction
-echo "1" > /sys/class/gpio/gpio18/value
+
--- drone_hat/drone.cpp
+++ drone_hat/drone.cpp
@@ -1,61 +1,28 @@
-/*
- * This file is part of the libopencm3 project.
- *
- * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
- * Copyright (C) 2011 Damjan Marion <damjan.marion@gmail.com>
- * Copyright (C) 2011 Mark Panajotovic <marko@electrontube.org>
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library.  If not, see <http://www.gnu.org/licenses/>.
- */
-
+#include <stdint.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/systick.h>
-
-uint32_t temp32;
 
 static void gpio_setup(void)
 {
 	rcc_periph_clock_enable(RCC_GPIOC);
-	/* Set GPIO8/9 (in GPIO port C) to 'output push-pull'. */
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO8);
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
-		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO9);
+	gpio_set(GPIOC, GPIO8);
+	gpio_clear(GPIOC, GPIO9);
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO8 | GPIO9);
 }
-
 
 int main(void)
 {
-	int i;
-
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	//rcc_clock_setup_in_hse_8mhz_out_72mhz();
 	gpio_setup();
 
-	temp32 = 0;
-
-	/* 72MHz / 8 => 9000000 counts per second */
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-
-	/* Set one LED for wigwag effect when toggling. */
-	gpio_set(GPIOC, GPIO8);
 
 	/* Blink the LEDs (PC8 and PC9) on the board. */
-	while (1) {
-		gpio_toggle(GPIOC, GPIO8 | GPIO9); /* Toggle LEDs. */
-		for (i = 0; i < 1000000; i++)      /* Wait a bit. */
+	while (1)
+	{
+		gpio_toggle(GPIOC, GPIO8);
+		gpio_toggle(GPIOC, GPIO9);
+
+		for (int i = 0; i < 100000; i++)
 			__asm__("nop");
 	}
 
