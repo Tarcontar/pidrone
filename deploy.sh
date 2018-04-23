ffe23970c839d34152672521bcd23724c8345351
feliwir <stephan.vedder@gmail.com>
2018-04-23 16:53:29 +0000

    current changes

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,10 @@ unexport ()
 	echo "unexport $1"
 	echo "$1" > /sys/class/gpio/unexport
 }
-unexport 18
-unexport 11
-unexport 10
-unexport 9
+#unexport 18
 bash compile.sh
 cd build
-make upload_drone_hat
+make upload
 #set Reset pin high
 echo "18" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio18/direction
--- drone_hat/drone.cpp
+++ drone_hat/drone.cpp
@@ -1,56 +1,61 @@
-#include <stdint.h>
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ * Copyright (C) 2011 Damjan Marion <damjan.marion@gmail.com>
+ * Copyright (C) 2011 Mark Panajotovic <marko@electrontube.org>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-#include <libopencm3/cm3/nvic.h>
-#include <libopencm3/cm3/systick.h>
 
-#include "io/serial.h"
+/* Set STM32 to 24 MHz. */
+static void clock_setup(void)
+{
+	rcc_clock_setup_in_hse_8mhz_out_24mhz();
 
-Serial serial;
-uint32_t temp32;
+	/* Enable GPIOC clock. */
+	rcc_periph_clock_enable(RCC_GPIOC);
+}
 
 static void gpio_setup(void)
 {
-	rcc_periph_clock_enable(RCC_GPIOC);
-
-	gpio_set(GPIOA, GPIO8); /* LED0 off */
-	gpio_clear(GPIOA, GPIO9); /* LED1 off */
-	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO8 | GPIO9);
-
-}
-
-void sys_tick_handler(void)
-{
-	temp32++;
-
-	/* We call this handler every 1ms so 1000ms = 1s on/off. */
-	if (temp32 == 1000) {
-		gpio_toggle(GPIOC, GPIO8); /* LED2 on/off */
-		gpio_toggle(GPIOC, GPIO9); /* LED2 on/off */
-		temp32 = 0;
-	}
+	/* Set GPIO8/9 (in GPIO port C) to 'output push-pull'. */
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO8);
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO9);
 }
 
 int main(void)
 {
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
+	int i;
+
+	clock_setup();
 	gpio_setup();
 
-	temp32 = 0;
+	/* Set one LED for wigwag effect when toggling. */
+	gpio_set(GPIOC, GPIO8);
 
-	/* 72MHz / 8 => 9000000 counts per second */
-	systick_set_clocksource(STK_CSR_CLKSOURCE_AHB_DIV8);
-
-	/* 9000000/9000 = 1000 overflows per second - every 1ms one interrupt */
-	/* SysTick interrupt every N clock pulses: set reload to N-1 */
-	systick_set_reload(8999);
-
-	systick_interrupt_enable();
-
-	/* Start counting. */
-	systick_counter_enable();
-
-	while (1); /* Halt. */
+	/* Blink the LEDs (PC8 and PC9) on the board. */
+	while (1) {
+		gpio_toggle(GPIOC, GPIO8 | GPIO9); /* Toggle LEDs. */
+		for (i = 0; i < 1000000; i++)      /* Wait a bit. */
+			__asm__("nop");
+	}
 
 	return 0;
 }
