bc1bec82cf5a930f31b8fd728d5937bce8288892
feliwir <stephan.vedder@gmail.com>
2018-04-29 20:58:15 +0000

    work on usart testcode

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	MCU::setup();
+	//MCU::setup();
 
 	/*
 	Serial ser(9600);
@@ -25,33 +25,40 @@ int main(void)
 		ser << "This is a test" << ser.endl;
 	});
 	*/
-	
+
+	rcc_clock_setup_in_hse_8mhz_out_24mhz();
+
 	rcc_periph_clock_enable(RCC_GPIOA);
-	rcc_periph_clock_enable(RCC_USART1);
-	
-	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
-	
-	usart_set_databits(USART1, 8);
-	usart_set_stopbits(USART1, USART_STOPBITS_1);
-	usart_set_mode(USART1, USART_MODE_TX);
-	usart_set_parity(USART1, USART_PARITY_NONE);
-	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-	
-	//usart_set_baudrate(USART1, 9600);
-	USART_BRR (USART1) = (uint16_t) ((24000000 << 4) / (9600 * 16));
-	
+
+	rcc_periph_clock_enable(RCC_USART2);
+
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART2_TX);
+
+	usart_set_databits(USART2, 8);
+	usart_set_stopbits(USART2, USART_STOPBITS_1);
+	usart_set_mode(USART2, USART_MODE_TX);
+	usart_set_parity(USART2, USART_PARITY_NONE);
+	usart_set_flow_control(USART2, USART_FLOWCONTROL_NONE);
+
+	usart_set_baudrate(USART2, 9600); //this function doesnt support 24 mhz yet
+	//USART_BRR (USART1) = (uint16_t) ((24000000 << 4) / (9600 * 16));
+
 	while(1)
 	{
 		//keep this for future testing
-		uint32_t delay = 5000000;
+		uint32_t delay = 500000;
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("NOP");
-		usart_send_blocking(USART1, 0xff);
+		usart_send_blocking(USART2, '0');
+		usart_send_blocking(USART2, '1');
+		//usart_send_blocking(USART1, '\r');
+		//usart_send_blocking(USART1, '\n');
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("NOP");
-		usart_send_blocking(USART1, 0xff);
-		
+		usart_send_blocking(USART2, '2');
+		usart_send_blocking(USART2, '3');
+		//usart_send_blocking(USART1, '\r');
+		//usart_send_blocking(USART1, '\n');
 	}
-
 	return 0;
 }
--- listen.py
+++ listen.py
@@ -8,9 +8,10 @@ ser = serial.Serial("/dev/ttyS0", 9600, timeout=10)
 print("Start listening:")
 while 1:
 	try:
-		text = ser.readline()[:-2].decode().strip()
-		if (len(text) > 0):
-			print (text)
+		print(ser.read())
+		#text = ser.readline()[:-2].decode().strip()
+		#if (len(text) > 0):
+		#	print (text)
 		time.sleep(0.05)
 	except KeyboardInterrupt:
 		print("Canceling listen")
