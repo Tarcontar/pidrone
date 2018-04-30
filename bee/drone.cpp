92f61ac7392ab7fb217d1bff31c617171eb6452f
Tarcontar <michael-schnabel@gmx.net>
2018-04-30 10:50:05 +0200

    moved usart code back

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -5,44 +5,19 @@
 #include <stdint.h>
 #include <string>
 
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/usart.h>
-
 int main(void)
 {
-	//MCU::setup();
+	MCU::setup();
 
-	/*
 	Serial ser(9600);
 	ser << "System starting" << ser.endl;
 
-	Motors motors;
-	motors.setupESCs();
-
+	/*
 	Handler::queryTask([ser]()
 	{
 		ser << "This is a test" << ser.endl;
 	});
 	*/
-
-	rcc_clock_setup_in_hse_8mhz_out_24mhz();
-
-	rcc_periph_clock_enable(RCC_GPIOA);
-
-	rcc_periph_clock_enable(RCC_USART1);
-
-	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
-
-	usart_set_databits(USART1, 8);
-	usart_set_stopbits(USART1, USART_STOPBITS_1);
-	usart_set_mode(USART1, USART_MODE_TX);
-	usart_set_parity(USART1, USART_PARITY_NONE);
-	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-
-	usart_set_baudrate(USART1, 9600);
-
-	usart_enable(USART1);
 
 	while(1)
 	{
@@ -50,16 +25,10 @@ int main(void)
 		uint32_t delay = 500000;
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("NOP");
-		usart_send_blocking(USART1, '0');
-		usart_send_blocking(USART1, '1');
-		usart_send_blocking(USART1, '\r');
-		usart_send_blocking(USART1, '\n');
+		ser << "test1" << ser.endl;
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("NOP");
-		usart_send_blocking(USART1, '2');
-		usart_send_blocking(USART1, '3');
-		usart_send_blocking(USART1, '\r');
-		usart_send_blocking(USART1, '\n');
+		ser << "test2" << ser.endl;
 	}
 	return 0;
 }
--- bee/io/usart.cpp
+++ bee/io/usart.cpp
@@ -12,29 +12,26 @@ void USART::begin(int baud)
 {
 	if (!m_ready)
 	{
-		rcc_periph_clock_enable(_USART_RCC_PORT); 
-		rcc_periph_clock_enable(_USART_RCC_USART_PORT);
+		rcc_periph_clock_enable(RCC_GPIOA); 
+		rcc_periph_clock_enable(RCC_USART1);
 
-		gpio_set_mode(_USART_PORT, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, _USART_TX);
+		gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
 
-		usart_set_databits(_USART, 8);
-		usart_set_stopbits(_USART, USART_STOPBITS_1);
-		usart_set_mode(_USART, USART_MODE_TX);
-		usart_set_parity(_USART, USART_PARITY_NONE);
-		usart_set_flow_control(_USART, USART_FLOWCONTROL_NONE);
+		usart_set_databits(USART1, 8);
+		usart_set_stopbits(USART1, USART_STOPBITS_1);
+		usart_set_mode(USART1, USART_MODE_TX);
+		usart_set_parity(USART1, USART_PARITY_NONE);
+		usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
 		
 		m_ready = true;
 	}
 	
-	//did not work the last time, test first
-	//usart_set_baudrate(_USART, baud);
-	USART_BRR(_USART) = (uint16_t) ((24000000 << 4) / (baud * 16)); 
-	
-	usart_enable(_USART);
+	usart_set_baudrate(USART1, baud);
+	usart_enable(USART1);
 }
 
 void USART::write(uint16_t data)
 {
-	usart_send_blocking(_USART, data);
+	usart_send_blocking(USART1, data);
 }
 
