99296b3158cde9d47b409b1eb5de9db708eaf2df
feliwir <stephan.vedder@gmail.com>
2019-03-12 10:34:24 +0100

    Changes for stm32 l4

--- cmake/stm32-toolchain.cmake
+++ cmake/stm32-toolchain.cmake
@@ -12,9 +12,9 @@ set(CMAKE_OBJCOPY ${TRIPLE}-objcopy)
 set(CMAKE_OBJDUMP ${TRIPLE}-objdump)
 #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${LINKER_SCRIPT}")
 
-set(SHARED_FLAGS "-specs=nano.specs -specs=nosys.specs -mthumb -mcpu=cortex-m3 -msoft-float -fshort-wchar")
+set(SHARED_FLAGS "-specs=nano.specs -specs=nosys.specs -mthumb -mcpu=cortex-m4 -msoft-float -fshort-wchar")
 set(CMAKE_C_FLAGS "${SHARED_FLAGS} -Os -g -Wall -s")
 set(CMAKE_CXX_FLAGS "${SHARED_FLAGS} -Os -fno-builtin -Wall -s")
 set(CMAKE_ASM_FLAGS "${SHARED_FLAGS} -g -Wa,--no-warn -x assembler-with-cpp")
 
-SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-wchar-size-warning -Wl,--gc-sections -mthumb -mcpu=cortex-m3 -nostartfiles -L${LINKER_SCRIPT_DIR}")
+SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-wchar-size-warning -Wl,--gc-sections -mthumb -mcpu=cortex-m4 -nostartfiles -L${LINKER_SCRIPT_DIR}")
