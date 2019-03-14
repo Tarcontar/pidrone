38b35edcd46ef081b80318a78a9e6f71e00ef882
Tarcontar <michael-schnabel@gmx.net>
2019-03-14 20:35:21 +0100

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,26 @@
 #!/usr/bin/env bash
 echo "Installing dependencies"
-sudo apt install git gcc-arm-none-eabi cmake openocd
+sudo apt install python git gcc-arm-none-eabi cmake openocd
 echo "Cloning repository"
 cd ~
 git clone --recursive https://github.com/Tarcontar/pidrone.git
-cd pidrone
+echo "update submodules"
+cd pidrone/contrib/tinygps
+git checkout master
+git pull 
+cd ../NemaTode
+git checkout master
+git pull
+cd ../bme280
+git checkout master
+git pull
+cd ../bmi160
+git checkout master
+git pull
+cd ../libopencm3
+git checkout master
+git pull
+cd ../..
 echo "Building project"
 mkdir build && cd build 
 cmake .. -DCMAKE_TOOLCHAIN_FILE="../cmake/stm32-toolchain.cmake"
