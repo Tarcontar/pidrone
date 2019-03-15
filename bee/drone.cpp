3151f9f18e4136841b4088c851234fb020dd0871
Tarcontar <michael-schnabel@gmx.net>
2019-03-15 18:11:21 +0100

    changes to bootstrap and added blink

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -3,6 +3,7 @@
 #include "io/sensors.h"
 #include "sys/mcu.h"
 #include "sys/handler.h"
+#include "sys/status.h"
 #include <stdint.h>
 #include <string>
 
@@ -12,12 +13,9 @@ int main(void)
 
 	ser << "Starting MCU..." << ser.endl;
 
-	Sensors sensors;
-	if (!sensors.setup())
-		return -1;
-
 	while(1)
 	{
+		Status::update();
 		//keep this for future testing
 		uint32_t delay = 4500000;
 		for (uint32_t i = 0; i < delay; i++)
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,26 +4,12 @@ echo "Installing dependencies"
 sudo apt install python git gcc-arm-none-eabi cmake openocd
 echo "Cloning repository"
 cd ~
-git clone --recursive https://github.com/Tarcontar/pidrone.git
+git clone --recurse-submodules https://github.com/Tarcontar/pidrone.git
 echo "update submodules"
-cd pidrone/contrib/tinygps
-git checkout master
-git pull 
-cd ../NemaTode
-git checkout master
-git pull
-cd ../bosch/bme280
-git checkout master
-git pull
-cd ../bmi160
-git checkout master
-git pull
-cd ../../libopencm3
-git checkout master
-git pull
-cd ../..
+cd pidrone
+git submodule update
 echo "Building project"
-mkdir build && cd build 
-cmake .. -DCMAKE_TOOLCHAIN_FILE="../cmake/stm32-toolchain.cmake"
-cmake --build .
+mkdir build && cd build
+cmake ..
+make all
 echo "Finished now"
