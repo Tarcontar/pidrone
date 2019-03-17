d8b1b307dbce794c3473f4396d6c6fc2149407de
Michael Schnabel <michael-schnabel@gmx.net>
2019-03-17 18:45:11 +0100

    compile openocd

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get upgrade
 rm -rf pidrone
 echo "Installing dependencies"
-sudo apt install python git gcc-arm-none-eabi cmake openocd
+sudo apt install python python-pip git gcc-arm-none-eabi cmake openocd autoconf libtool make pkg-config libusb-1.0-0 libusb-1.0-0-dev
+pip install pyserial
+echo "build openocd"
+git clone http://openocd.zylin.com/openocd
+cd openocd
+./bootstrap
+./configure --enable-sysfsgpio --enable-bcm2835gpio
+make
+sudo make install
 echo "Cloning repository"
 cd ~
 git clone --recurse-submodules https://github.com/Tarcontar/pidrone.git
 echo "Building project"
 cd pidrone
-mkdir build && cd build
+mkdir build
+cd build
 cmake ..
 make all
 echo "Finished now"
