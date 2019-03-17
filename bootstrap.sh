3c04ee4c2e514b65cb543164307793c0bc88adf8
Michael Schnabel <michael-schnabel@gmx.net>
2019-03-17 21:20:31 +0100

    some fixes

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 sudo apt-get update
 sudo apt-get upgrade
-rm -rf pidrone
 echo "Installing dependencies"
 sudo apt install python python-pip git gcc-arm-none-eabi cmake autoconf libtool make pkg-config libusb-1.0-0 libusb-1.0-0-dev
 pip install pyserial
 echo "build openocd"
+cd ~
 rm -rf openocd
 git clone http://openocd.zylin.com/openocd
 cd openocd
@@ -15,6 +15,7 @@ make
 sudo make install
 echo "Cloning repository"
 cd ~
+rm -rf pidrone
 git clone --recurse-submodules https://github.com/Tarcontar/pidrone.git
 echo "Building project"
 cd pidrone
