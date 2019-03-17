7efb376dae2fba85c7c5c2f05febed9bef3a4915
Tarcontar <michael-schnabel@gmx.net>
2019-03-17 21:22:23 +0000

    some work on blink

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -1,25 +1,32 @@
-#include "actuators/motors.h"
+//#include "actuators/motors.h"
 //#include "io/serial.h"
-#include "io/sensors.h"
-#include "sys/mcu.h"
+//#include "io/sensors.h"
+//#include "sys/mcu.h"
 //#include "sys/handler.h"
-#include "sys/status.h"
-#include <stdint.h>
+//#include "sys/status.h"
+//#include <stdint.h>
 
-int main(void)
+#include "hat_pcb.h"
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/flash.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/cm3/systick.h>
+
+int main()
 {
-	MCU::setup();
-
-	//ser << "Starting MCU..." << ser.endl;
+	rcc_periph_clock_enable(RCC_GPIOC);
+	gpio_mode_setup(RCC_GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+	gpio_set(RCC_GPIOC, GPIO12);
 
 	while(1)
 	{
-		Status::update();
+		//gpio_toggle(_LED_STATUS_PORT, _LED_STATUS_PIN);
+		//Status::update();
 		//keep this for future testing
-		uint32_t delay = 4500000;
+		uint32_t delay = 45000;
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("NOP");
-		float t = 2.75;
 		//ser << "Hi from Serial! " << 545 << " " << 3.14 << " " << t << ser.endl;
 	}
 	return 0;
