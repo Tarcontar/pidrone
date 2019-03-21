d93f5f72e0f505cb3fcf244e09f97d13a9462303
Tarcontar <michael-schnabel@gmx.net>
2019-03-21 10:51:43 +0100

    updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-sudo apt-get update
-sudo apt-get upgrade
 echo "Installing dependencies"
 sudo apt install python python-pip git gcc-arm-none-eabi cmake autoconf libtool make pkg-config libusb-1.0-0 libusb-1.0-0-dev
 pip install pyserial
@@ -16,7 +14,7 @@ sudo make install
 echo "Cloning repository"
 cd ~
 rm -rf pidrone
-git clone --recurse-submodules https://github.com/Tarcontar/pidrone.git
+git clone --recursive https://github.com/Tarcontar/pidrone.git
 echo "Building project"
 cd pidrone
 mkdir build
