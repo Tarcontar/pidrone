2782606d4d999f82d76e1669b97318652678d72f
Tarcontar <michael-schnabel@gmx.net>
2019-06-15 18:16:42 +0100

    changed fpv version from 5 to 4

--- cmake/stm32-toolchain.cmake
+++ cmake/stm32-toolchain.cmake
@@ -12,9 +12,9 @@ set(CMAKE_OBJCOPY ${TRIPLE}-objcopy)
 set(CMAKE_OBJDUMP ${TRIPLE}-objdump)
 #set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -T ${LINKER_SCRIPT}")
 
-set(SHARED_FLAGS "-specs=nano.specs -specs=nosys.specs -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -fshort-wchar")
+set(SHARED_FLAGS "-specs=nano.specs -specs=nosys.specs -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fshort-wchar")
 set(CMAKE_C_FLAGS "${SHARED_FLAGS} -Os -g -Wall -s")
 set(CMAKE_CXX_FLAGS "${SHARED_FLAGS} -Os -fno-builtin -Wall -s")
 set(CMAKE_ASM_FLAGS "${SHARED_FLAGS} -g -Wa,--no-warn -x assembler-with-cpp")
 
-SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-wchar-size-warning -Wl,--gc-sections -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv5-sp-d16 -nostartfiles -L${LINKER_SCRIPT_DIR} -T ${CMAKE_CURRENT_LIST_DIR}/linker.ld")
+SET(CMAKE_EXE_LINKER_FLAGS "-Wl,--no-wchar-size-warning -Wl,--gc-sections -mthumb -mcpu=cortex-m4 -mfloat-abi=hard -mfpu=fpv4-sp-d16 -nostartfiles -L${LINKER_SCRIPT_DIR} -T ${CMAKE_CURRENT_LIST_DIR}/linker.ld")
