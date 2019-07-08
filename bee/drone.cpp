09f125a31469b1edf8e3ccfa6294c27261c1599d
Tarcontar <michael-schnabel@gmx.net>
2019-07-08 21:46:11 +0100

    first blink on drone hat

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -5,18 +5,15 @@
 int main()
 {
 	//status led
-
-	rcc_periph_clock_enable(RCC_GPIOA);
-
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
-
-	gpio_set(GPIOA, GPIO5);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO12);
+	gpio_set(GPIOC, GPIO12);
 
 	while(1)
 	{
-		//gpio_toggle(GPIOA, GPIO5);
+		gpio_toggle(GPIOC, GPIO12);
 		//keep this for future testing
-		uint32_t delay = 100000;
+		uint32_t delay = 360000;
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("nop");
 	}
