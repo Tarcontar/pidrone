b8b1f0bc2680ca333b79e9071320c129a4c60d4c
Tarcontar <michael-schnabel@gmx.net>
2019-07-10 21:31:31 +0200

    added external clock setup

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -12,8 +12,30 @@ static void setup_internal_clock()
 	flash_dcache_enable();
 	flash_icache_enable();
 	
-	// 8mhz / 2 => 4 * 40 => 160mhz / 2 => 80 mhz
+	// 16mhz / 4 => 4 * 40 => 160mhz / 2 => 80 mhz
 	rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 4, 40, 0, 0, RCC_PLLCFGR_PLLR_DIV2);
+	rcc_osc_on(RCC_PLL);
+	rcc_wait_for_osc_ready(RCC_PLL);
+	
+	rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
+	rcc_wait_for_sysclk_status(RCC_PLL);
+	
+	rcc_ahb_frequency = 80e6;
+	rcc_apb1_frequency = 80e6;
+	rcc_apb2_frequency = 80e6;
+}
+
+static void setup_external_clock()
+{
+	rcc_osc_on(RCC_HSE);
+	
+	flash_prefetch_enable();
+	flash_set_ws(4);
+	flash_dcache_enable();
+	flash_icache_enable();
+	
+	// 8mhz / 2 => 4 * 40 => 160mhz / 2 => 80 mhz
+	rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSE, 2, 40, 0, 0, RCC_PLLCFGR_PLLR_DIV2);
 	rcc_osc_on(RCC_PLL);
 	rcc_wait_for_osc_ready(RCC_PLL);
 	
@@ -28,6 +50,7 @@ static void setup_internal_clock()
 int main()
 {
 	setup_internal_clock();
+	setup_external_clock();
 
 	//status led
 	rcc_periph_clock_enable(LED_STATUS_RCC_PORT);
