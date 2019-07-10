bf38096f0f7a478cbd2307bb705c3e50873d83fb
Tarcontar <michael-schnabel@gmx.net>
2019-07-10 20:40:13 +0100

    got external clock setup working

--- bee/drone.cpp
+++ bee/drone.cpp
@@ -3,29 +3,7 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/flash.h>
 
-static void setup_internal_clock()
-{
-	rcc_osc_on(RCC_HSI16);
-	
-	flash_prefetch_enable();
-	flash_set_ws(4);
-	flash_dcache_enable();
-	flash_icache_enable();
-	
-	// 16mhz / 4 => 4 * 40 => 160mhz / 2 => 80 mhz
-	rcc_set_main_pll(RCC_PLLCFGR_PLLSRC_HSI16, 4, 40, 0, 0, RCC_PLLCFGR_PLLR_DIV2);
-	rcc_osc_on(RCC_PLL);
-	rcc_wait_for_osc_ready(RCC_PLL);
-	
-	rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
-	rcc_wait_for_sysclk_status(RCC_PLL);
-	
-	rcc_ahb_frequency = 80e6;
-	rcc_apb1_frequency = 80e6;
-	rcc_apb2_frequency = 80e6;
-}
-
-static void setup_external_clock()
+static void setup_clock()
 {
 	rcc_osc_on(RCC_HSE);
 	
@@ -41,16 +19,11 @@ static void setup_external_clock()
 	
 	rcc_set_sysclk_source(RCC_CFGR_SW_PLL);
 	rcc_wait_for_sysclk_status(RCC_PLL);
-	
-	rcc_ahb_frequency = 80e6;
-	rcc_apb1_frequency = 80e6;
-	rcc_apb2_frequency = 80e6;
 }
 
 int main()
 {
-	setup_internal_clock();
-	setup_external_clock();
+	setup_clock();
 
 	//status led
 	rcc_periph_clock_enable(LED_STATUS_RCC_PORT);
@@ -61,7 +34,7 @@ int main()
 	{
 		gpio_toggle(LED_STATUS_PORT, LED_STATUS_PIN);
 		//keep this for future testing
-		uint32_t delay = 800000;
+		uint32_t delay = 8000000;
 		for (uint32_t i = 0; i < delay; i++)
 			__asm__("nop");
 	}
